ae41bef366da609e0a99760010341b188c2e2080
alicia-castillo <alicia-castilloo@outlook.com>
2019-02-06 18:49:12 -0600

    Create Practica2.js

---
+++ Practica2.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+/*app.get('/:name', (req, res) => {
+    console.log(req.query);
+    console.log(req.params);
+    res.send(`<h3>Hello ${req.params.name}!</h3>`)
+});*/
+
+app.get('/json', (req,res)=>{
+    res.json ({'name' : 'Cucho'});
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
