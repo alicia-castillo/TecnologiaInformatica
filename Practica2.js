a55e1b14278b7e635f86e9dd8b1d15f52e4c238b
alicia-castillo <alicia-castilloo@outlook.com>
2019-02-06 18:55:07 -0600

    Update Practica2.js

--- Practica2.js
+++ Practica2.js
@@ -12,4 +12,8 @@ app.get('/json', (req,res)=>{
     res.json ({'name' : 'Cucho'});
 });
 
+app.post('/json',(req,res)=>{
+    res.json({'name':'Jelly'});
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
